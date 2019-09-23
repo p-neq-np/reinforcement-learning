becf919c3e8d6afa1f72fa18e2b3f5d5b5ef029a
p-neq-np <3762981+p-neq-np@users.noreply.github.com>
2019-09-23 02:05:19 +0200

    minor

--- Delivery_Optimization/route_db.py
+++ Delivery_Optimization/route_db.py
@@ -3,7 +3,7 @@ import pymongo
 # simplified for the purpose
 class Storage:
 
-    def __init__(self, coll_name=""):
+    def __init__(self, coll_name="tsp_test"):
         self.coll_name = coll_name
         self._init_client() # connection
         self._init_db() # database
@@ -13,7 +13,7 @@ class Storage:
         # the connector
         self.client = pymongo.MongoClient("mongodb://localhost:27017/")
 
-    def _init_db(self, name = "db_routes"):
+    def _init_db(self, name = "route_db"):
         # creates the db if it doesn't exist
         self.db = self.client[name]
 
@@ -28,15 +28,15 @@ class Storage:
         """
         return self.collection.insert_one(route_dict)
 
-    def export_as_CSV(self, filename=None):
+    def export_as_CSV(self, collection_name=None, field_name=None filename=None):
         """
-        :param filename: full path of the CSV
+        :param collection_name: test table,filename: full path of the CSV
         :return: returns nothing
         """
-        if filename is None:
-            pass
+        if filename and field_name and collection_name is not None:
+            # subprocess
         else:
-            pass
+            print("Parameters are not valid")
 
     # simple print
     def print_table(self):
